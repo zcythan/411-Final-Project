527620b38f65d9e50fada996c803eee1744db718
zcythan <jferrick@umich.edu>
2023-12-10 17:27:10 -0500

    loading all dataset with multi-threading

--- lieDetector.py
+++ lieDetector.py
@@ -1,7 +1,12 @@
+from transformers import BertTokenizer, BertForSequenceClassification
+
+
 class lieDetector:
 
     def __init__(self):
         self.__var = 0
+        tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+        model = BertForSequenceClassification.from_pretrained('bert-base-uncased')
 
     def load(self):
         #code to load in model here
